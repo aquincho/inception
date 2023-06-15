03a855a404abceb6ce85fac6640d4e56dd633a8b
Arnaud QUINCHON <aquincho@bess-f4r6s4.clusters.42paris.fr>
2023-06-15 12:13:41 +0200

    fix: mariadb socket (in progress1)

--- srcs/requirements/mariadb/tools/mariadb.sh
+++ srcs/requirements/mariadb/tools/mariadb.sh
@@ -1,12 +1,26 @@
 #!/bin/sh
 
+mkdir -p /home/aquincho/data/wordpress
+mkdir -p /home/aquincho/data/db
+
 service mysql start;
 
-mysql -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DB}\`;"
-mysql -e "CREATE USER IF NOT EXISTS \`${MARIADB_USER}\`@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';"
-mysql -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DATABASE}\`.* TO \`${MARIADB_USER}\`@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
-mysql -e "FLUSH PRIVILEGES;"
+#/usr/bin/mysql_install_db --user=root --basedir=/usr --datadir=/var/lib/mysql
+#/usr/bin/mysqld --user=root --datadir=/var/lib/mysql & sleep 2
+#service mysql start;
 
-pkill mysqld
-/usr/bin/mysqld --user=root --datadir=/var/lib/mysql
+#mysql -e "CREATE DATABASE IF NOT EXISTS \`${MARIADB_DBNAME}\`;"
+#mysql -e "CREATE USER IF NOT EXISTS \`${MARIADB_USER}\`@'localhost' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+#mysql -e "GRANT ALL PRIVILEGES ON \`${MARIADB_DBNAME}\`.* TO \`${MARIADB_USER}\`@'%' IDENTIFIED BY '${MARIADB_PASSWORD}';"
+#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${MARIADB_ROOT_PASSWORD}';"
+#mysql -e "FLUSH PRIVILEGES;"
+
+mysql -uroot -e "CREATE DATABASE IF NOT EXISTS db;"
+#mysql -uroot -e "CREATE USER IF NOT EXISTS \`user\`@'localhost' IDENTIFIED BY 'pwd';"
+#mysql -uroot -e "GRANT ALL PRIVILEGES ON \`db\`.* TO \`user\`@'%' IDENTIFIED BY 'pwd';"
+#mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY 'rt_pwd';"
+#mysql -uroot -e "FLUSH PRIVILEGES;"
+
+mysqladmin -uroot shutdown
+#exec mysqld_safe
+#/usr/bin/mysqld --user=root --datadir=/var/lib/mysql
